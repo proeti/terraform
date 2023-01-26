52260fea4d4e2a49bcf9207b6b3b78e41243afcc
Thiago Proeti Nunes <proeti@gmail.com>
2023-01-26 17:00:25 -0300

    deploy

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     digitalocean = {
-      source = "digitalocean/digitalocean"
+      source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
   }
@@ -9,27 +9,27 @@ terraform {
 
 # Configure the DigitalOcean Provider
 provider "digitalocean" {
-  token = "dop_v1_aaf828b840573a2f22890be45eb1f9bf4d483989750d8db218e340ef664f2502"
+  token = var.do_token
 }
 
 # Retrieve existing ssh key
 data "digitalocean_ssh_key" "DevOps" {
-  name = "DevOps"
+  name = var.ssh_key
 }
 
 # Create a new Web Droplet in the nyc1 region
 resource "digitalocean_droplet" "ubuntu-jenkins" {
-  image  = "ubuntu-22-04-x64"
-  name   = "ubuntu-jenkins"
-  region = "nyc1"
-  size   = "s-2vcpu-2gb"
+  image    = "ubuntu-22-04-x64"
+  name     = "ubuntu-jenkins"
+  region   = "nyc1"
+  size     = "s-2vcpu-2gb"
   ssh_keys = [data.digitalocean_ssh_key.DevOps.id]
 }
 
 # Kubernetes Cluster Provisioning
 resource "digitalocean_kubernetes_cluster" "k8s-devops" {
-  name   = "k8s-devops"
-  region = "nyc1"
+  name    = "k8s-devops"
+  region  = var.region
   version = "1.25.4-do.0"
 
   node_pool {
@@ -37,4 +37,25 @@ resource "digitalocean_kubernetes_cluster" "k8s-devops" {
     size       = "s-2vcpu-2gb"
     node_count = 2
   }
+}
+
+variable "region" {
+  default = ""
+}
+
+variable "do_token" {
+  default = ""
+}
+
+variable "ssh_key" {
+  default = ""
+}
+
+output "jenkins_ip" {
+  value = digitalocean_droplet.ubuntu-jenkins.ipv4_address
+}
+
+resource "local_file" "name" {
+  content  = digitalocean_kubernetes_cluster.k8s-devops.kube_config.0.raw_config
+  filename = "kube_config.yaml"
 }
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+region   = "nyc1"
+do_token = "dop_v1_d229b75a2bd98c5c3e630d52470ea9a1ac2ba651141c5e03561950938d68f137"
+ssh_key  = "DevOps"
