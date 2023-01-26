4142d2b534896d3d786d652a1c51f6d9d8d4438d
Thiago Proeti Nunes <proeti@gmail.com>
2023-01-26 16:26:06 -0300

    deploy kubernetes

--- main.tf
+++ main.tf
@@ -9,7 +9,12 @@ terraform {
 
 # Configure the DigitalOcean Provider
 provider "digitalocean" {
-  token = "dop_v1_cda458688f8db3f572104fa567697ea01ed9fae5d6602c4aa9d1791c493ea938"
+  token = "dop_v1_aaf828b840573a2f22890be45eb1f9bf4d483989750d8db218e340ef664f2502"
+}
+
+# Retrieve existing ssh key
+data "digitalocean_ssh_key" "DevOps" {
+  name = "DevOps"
 }
 
 # Create a new Web Droplet in the nyc1 region
@@ -18,4 +23,18 @@ resource "digitalocean_droplet" "ubuntu-jenkins" {
   name   = "ubuntu-jenkins"
   region = "nyc1"
   size   = "s-2vcpu-2gb"
+  ssh_keys = [data.digitalocean_ssh_key.DevOps.id]
+}
+
+# Kubernetes Cluster Provisioning
+resource "digitalocean_kubernetes_cluster" "k8s-devops" {
+  name   = "k8s-devops"
+  region = "nyc1"
+  version = "1.25.4-do.0"
+
+  node_pool {
+    name       = "worker-pool"
+    size       = "s-2vcpu-2gb"
+    node_count = 2
+  }
 }
